639a7f65d4300be5241358dd69fbd5de1ccbabae
Abhijeet Chopra <abhijeetchopra@yahoo.com>
2017-10-12 19:58:57 -0500

    Create generateListFromFiles.bat

---
+++ generateListFromFiles.bat
@@ -0,0 +1,18 @@
+::Auth: Abhijeet Chopra 
+::Date: Feb 28,2017
+::Prog: !FileListGenerator.bat
+::Desc: Batch file to write dir file list to text file
+
+@echo off
+setlocal
+
+:PROMPT
+echo This program will generate list of files in current directory.
+SET /P req=Do you wish to proceed? (Y/[N])?
+IF /I "%req%" NEQ "Y" GOTO END
+
+dir *.docx /b > !FileList.txt
+start notepad !FileList.txt
+
+:END
+endlocal
